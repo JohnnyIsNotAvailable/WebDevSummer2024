1c9624791e7f81d482dbcb44355c0a53547ad056
JohnnyIsNotAvailable <kabevzhanbek@gmail.com>
2024-06-14 18:25:51 +0500

    m

---
+++ Lab6/src/App.js
@@ -0,0 +1,23 @@
+// import
+import React, { useState } from 'react';
+import Home from "./components/home/home";
+import { BrowserRouter as Router, Routes, Route } from 
+
+function App() {
+
+    return (
+        <div>
+            {/* {<FetchData/>} */}
+            <Home/>
+        </div>
+    );
+
+    function App() {
+
+        return { 
+            <div>
+            
+            </div>
+        }
+    }
+}
---
+++ Lab6/src/components/home/home.jsx
@@ -0,0 +1,12 @@
+import "./home.css"
+const Home = () => {
+    return(
+        <div className ="div_Block" >
+            sdfg
+        </div>
+
+
+    )
+}
+
+export default Home;
